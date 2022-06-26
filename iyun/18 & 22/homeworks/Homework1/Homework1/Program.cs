ae0a72bf0a0d84bae2bbcbffc7629abd1cf77114
hakimqarayev <hakim.qarayev20@gmail.com>
2022-06-26 19:16:37 +0400

    added new homeworks

---
+++ iyun/18 & 22/homeworks/Homework1/Homework1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Homework 1: İstifadəçi mənfi dəyər daxil edənə qədər,
+               daxil etdiyi bütün müsbət rəqəmləri toplayıb axırda nəticəni ekrana yazın.
+            */
+
+            int result = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Eded daxil edin:");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                if (a > 0)
+                {
+                    result += a;
+                }
+                else
+                    break;
+
+
+            }
+            Console.WriteLine("Netice: " + result);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}

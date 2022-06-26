ab3ce60e24dccf41f2e39c18e879810674ab601b
hakimqarayev <hakim.qarayev20@gmail.com>
2022-06-26 18:37:53 +0400

    revert

---
+++ iyun/15/homeworks/Homework1/Homework1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Homework 1:
+             Əvvəlcədən iki dəyişəndə username və password saxlayın. 
+             Bunlar heç bir zaman dəyişməyəcək şəkildə olsun. Yəni proqramda belə dəyişdirmək mümkün olmasın
+             Sonra istifadəçidən username və password'u daxil etməsini istəyin. 
+             Əgər istifadəçi doğru məlumatları yazsa ekrana "Sisteme daxil olundu." mesajı çıxsın.
+             Yox əgər səhv məlumat daxil etsə, yenidən username və password'u daxil etməsini istəyin. 
+             3 dəfə səhv daxil etdikdən sonra "Hesab bloklandi" yazsın.
+             */
+
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+
+            const string username = "hakimqarayev";
+            const string password = "123H#";
+
+            string enteredUsername, enteredPassword;
+
+            Console.WriteLine("username daxil edin:");
+             enteredUsername = Console.ReadLine();
+
+            Console.WriteLine("password daxil edin:");
+             enteredPassword = Console.ReadLine();
+
+            if (username == enteredUsername && password == enteredPassword)
+            {
+                Console.WriteLine("Sistemə daxil olundu.");
+            }
+            else
+            {
+                Console.WriteLine("Məlumatlar yanlışdır. \nYenidən cəhd edin(*2 şansınız var)");
+
+                Console.WriteLine("username daxil edin:");
+                enteredUsername = Console.ReadLine();
+
+                Console.WriteLine("password daxil edin:");
+                enteredPassword = Console.ReadLine();
+
+                if (username == enteredUsername && password == enteredPassword)
+                {
+                    Console.WriteLine("Məlumatlar duzgundur, Sistemə daxil oldunuz.");
+                }
+                else
+                {
+                    Console.WriteLine("Məlumatlar yanlışdır. \n Yenidən cəhd edin(*son ansınız qaldı)");
+
+                    Console.WriteLine("username daxil edin:");
+                    enteredUsername = Console.ReadLine();
+
+                    Console.WriteLine("password daxil edin:");
+                    enteredPassword = Console.ReadLine();
+
+                    if (username == enteredUsername && password == enteredPassword)
+                    {
+                        Console.WriteLine("Məlumatlar duzgundur, Sistemə daxil oldunuz.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hesabınız bloklandı.");
+                    }
+                }
+            }
+            
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ iyun/15/homeworks/task/task/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             (Kecen dersin 3-cu taskidir.)
+             1. İstifadəçidən ay'ı rəqəm olaraq alıb switch-case'dən istifadə edərək həmin ayın adını ekrana yazdırın. 
+             Məsələn 1 olsa yanvar, 5 olsa may.
+             2. İstifadəçi ölkənin adını daxil edəndə paytaxtının adını ekrana yazdırın.
+             10 dənə ölkə olsa kifayətdir.
+             */
+
+            //// DateTime date = Convert.ToDateTime(Console.ReadLine());
+
+
+            ////if (date.Month == 1)
+            //// {
+            ////     Console.WriteLine(date.ToString("MMMM"));
+            //// }
+            //// Console.ReadLine();
+
+
+            #region DateTime month name
+            DateTime date = Convert.ToDateTime(Console.ReadLine());
+
+            int dateTimeMonthName = Convert.ToInt32(date.Month);
+           
+            switch (dateTimeMonthName)
+            {
+                case 1:
+                    Console.WriteLine("yanvar");
+                    break;
+                case 2:
+                    Console.WriteLine("fevral");
+                    break;
+                case 3:
+                    Console.WriteLine("mart");
+                    break;
+                case 4:
+                    Console.WriteLine("aprel");
+                    break;
+                case 5:
+                    Console.WriteLine("may");
+                    break;
+                case 6:
+                    Console.WriteLine("iyun");
+                    break;
+                case 7:
+                    Console.WriteLine("iyul");
+                    break;
+                case 8:
+                    Console.WriteLine("avqust");
+                    break;
+                case 9:
+                    Console.WriteLine("sentyabr");
+                    break;
+                case 10:
+                    Console.WriteLine("oktyabr");
+                    break;
+                case 11:
+                    Console.WriteLine("noyabr");
+                    break;
+                case 12:
+                    Console.WriteLine("dekabr");
+                    break;
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+            Console.ReadLine();
+            #endregion
+
+
+
+
+            #region Countries
+
+            Console.WriteLine("Olke adi daxil edin:");
+            string countryName = Console.ReadLine();
+
+            switch (countryName)
+            {
+                case "Azerbaycan":
+                    Console.WriteLine("Baki");
+                    break;
+                case "Turkiye":
+                    Console.WriteLine("Ankara");
+                    break;
+                case "Ispaniya":
+                    Console.WriteLine("Madrid");
+                    break;
+                case "Italiya":
+                    Console.WriteLine("Roma");
+                    break;
+                case "Avstriya":
+                    Console.WriteLine("Vyana");
+                    break;
+                case "Belcika":
+                    Console.WriteLine("Brussel");
+                    break;
+                case "Cexiya":
+                    Console.WriteLine("Praqa");
+                    break;
+                case "Danimarka":
+                    Console.WriteLine("Kopenhagen");
+                    break;
+                case "Hindistan":
+                    Console.WriteLine("Dehli");
+                    break;
+
+
+
+
+                default:
+                    break;
+            }
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
